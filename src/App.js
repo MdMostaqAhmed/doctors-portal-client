52db164d25cae8eccf7d8fb424612b9deeaa1ec9
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2023-01-17 09:03:31 +0600

    install react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div>
-      <button className='btn btn-primary'>Button</button>
+      <Navbar></Navbar>
     </div>
   );
 }
