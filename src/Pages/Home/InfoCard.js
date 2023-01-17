1f23f70fc3ed03b42dccb34a2a81943a67775eb1
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2023-01-17 21:32:49 +0600

    add dynamic image and background color in CardInfo

--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const InfoCard = ({ img }) => {
+const InfoCard = ({ img, cardTitle, bgClass }) => {
     return (
-        <div className="card lg:card-side bg-base-100 shadow-xl bg-accent">
-            <figure><img src={img} alt="Album" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">New album is released!</h2>
+        <div className={`card lg:card-side bg-base-100 shadow-xl bg-accent ${bgClass}`}>
+            <figure className='pl-5'>
+                <img src={img} alt="Album" />
+            </figure>
+
+            <div className="card-body text-white">
+                <h2 className="card-title">{cardTitle}</h2>
                 <p>Click the button to listen on Spotiwhy app.</p>
 
             </div>
