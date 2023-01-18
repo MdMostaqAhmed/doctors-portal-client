0ad84e7c61148fdaabfeec72953d20775035ee88
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2023-01-18 19:23:16 +0600

    add service component and load data from json file

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,21 +1,17 @@
-import React, { useState } from 'react';
 import chair from '../../assets/images/chair.png'
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
-import { format } from 'date-fns';
 
 
-const AppointmentBanner = () => {
-    const [date, setDate] = useState(new Date());
+const AppointmentBanner = ({ date, setDate }) => {
+
     return (
-        <div className="hero min-h-screen">
+        <div className="hero ">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt='' />
                 <div>
                     <DayPicker mode="single" selected={date} onSelect={setDate} />
-                    <p className='py-5 font-bold text-center'>You have selected: {format(date, 'PP')}</p>
                 </div>
-
             </div>
         </div>
     );
