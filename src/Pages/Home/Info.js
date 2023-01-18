85d70fdfb02cf6784c789d2495a393a10f26deef
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2023-01-18 10:15:43 +0600

    add Services and Service component

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -7,9 +7,9 @@ import { accent } from 'daisyui/src/colors';
 
 const Info = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
             <InfoCard cardTitle="Opening Hours" bgClass='bg-gradient-to-r from-secondary to-primary' img={clock}></InfoCard>
-            <InfoCard cardTitle="Ours Location" bgClass='bg-accent' img={marker}></InfoCard>
+            <InfoCard cardTitle="Ours Location" bgClass='bg-slate-500' img={marker}></InfoCard>
             <InfoCard cardTitle="Contact Us" bgClass='bg-gradient-to-r from-secondary to-primary' img={phone}></InfoCard>
         </div>
     );
