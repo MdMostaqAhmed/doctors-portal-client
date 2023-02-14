42a3e68367df3e2de85aa10afaf535f20d340163
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2023-02-14 22:28:19 +0600

    add MyAppointments and MyReview components

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
         <div className="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col items-center justify-center">
-                {/* <!-- Page content here --> */}
+                <h2 className='text-5xl text-purple-500'>Dashboard</h2>
+                <Outlet></Outlet>
                 <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
 
             </div>
