9f2f191257f70084ad9c3a715adeb1a016ea91cd
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2023-02-15 20:44:02 +0600

    load data from booking api and show data to MyAppointments component

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -26,6 +26,7 @@ const Navbar = () => {
                     </div>
                     <a className="btn btn-ghost normal-case text-xl">Doctors Portal</a>
                 </div>
+
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
                         <li><Link to="/">Home</Link></li>
@@ -49,7 +50,16 @@ const Navbar = () => {
                     </ul>
                 </div>
 
+                <div className="navbar-end">
+                    <label htmlFor="dashboard-sidebar" tabIndex={1} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    {/* <label htmlFor="dashboard-sidebar" className="btn btn-primary drawer-button lg:hidden">Open drawer</label> */}
+                </div>
+
+
             </div>
+
         </div>
     );
 };
