33fda737b21eda2925eb1f70689328f5dfa252b5
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2023-02-19 14:20:02 +0600

    create RequireAdmin component

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -6,7 +6,7 @@ import useAdmin from '../../Hooks/useAdmin';
 
 
 const Dashboard = () => {
-    const user = useAuthState(auth);
+    const [user] = useAuthState(auth);
     const [admin] = useAdmin(user);
     return (
         <div className="drawer drawer-mobile">
