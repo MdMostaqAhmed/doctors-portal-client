f490fd9f591d640a93ea9b9d0f6a0c6c615ffc3c
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2023-02-19 12:31:31 +0600

    making admin power for all user only for admin

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../Shared/Loading';
+import UserRow from './UserRow';
 
 const Users = () => {
-    const { data, isLoading } = useQuery('users', () => fetch('http://localhost:5000/user').then(res => res.json()))
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user', {
+        method: 'GET',
+        headers: {
+            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()))
     if (isLoading) {
         return <Loading></Loading>
     }
     return (
         <div>
-            <h2 className='text-2xl'>All Users: {data.length}</h2>
+            <h2 className='text-2xl'>All Users: {users.length}</h2>
             <div className="overflow-x-auto">
                 <table className="table w-full">
 
@@ -22,14 +28,12 @@ const Users = () => {
                         </tr>
                     </thead>
                     <tbody>
-
-                        <tr>
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                        </tr>
-
+                        {
+                            users.map(user => <UserRow
+                                user={user}
+                                key={user._id}
+                                refetch={refetch}></UserRow>)
+                        }
                     </tbody>
                 </table>
             </div>
